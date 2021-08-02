7f7f37fda1a0c08b2276aff6a4f44b13baa06eb4
um41r254 <um41r.254@gmail.com>
2021-08-02 19:00:35 +0500

    error

--- app/src/main/java/com/mid_banchers/starting_again/MainActivity.java
+++ app/src/main/java/com/mid_banchers/starting_again/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,6 +18,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.mid_banchers.starting_again.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
@@ -27,7 +29,9 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    List<String> path;
+    String path;
+    Object pat;
+    
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -48,38 +52,54 @@ public class MainActivity extends AppCompatActivity {
 
         });
         binding.timeUpd.setOnClickListener(v -> {
-////            Map<String,Object> keyWord = new HashMap<>();
-////            keyWord.putIfAbsent("addedOn",null);
-//            Map<String,Object> data = new HashMap<>();
-//            data.put("addedOn",Timestamp.now());
-//            db.collection("Brands")
-//                    .document("abc")
-//                        .set(data)
+            Map<String,Object> data = new HashMap<>();
+            data.put("addedOn",Timestamp.now());
+            db.collection("Brands")
+                    .addSnapshotListener((value, error) -> {
+                        if (error != null) {
+                            Log.w("Fail", error);
+                        } else {
+
+                            Toast.makeText(this, "u", Toast.LENGTH_SHORT).show();
+
+
+                            for (QueryDocumentSnapshot ds : value) {
+
+                                db.collection("Brands")
+                                        .document(ds.getId())
+                                        .update(data);
+                                Toast.makeText(this, "g", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                        
+
+                    });
+//            db.collection("Brands").get().
+//                    .document(path)
+//                        .update(data)
 //                        .addOnSuccessListener(new OnSuccessListener<Void>() {
 //                            @Override
 //                            public void onSuccess(Void aVoid) {
 //                                Toast.makeText(MainActivity.this, "Updated", Toast.LENGTH_SHORT).show();
 //                            }
-//                        });
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                @Override
+//                public void onFailure(@NonNull Exception e) {
+//                    Map<String,Timestamp> time = new HashMap<>();
+//                    time.put("addedOn",null);
+//                    db.collection("Brands")
+//                            .document()
+//                            .set(time);
+//
+//
+//                    Toast.makeText(MainActivity.this, "Fialed", Toast.LENGTH_SHORT).show();
+//                }
+//            });
 
 
-        Map<String, Object> data = new HashMap<>();
 
-            data.put("secondName", "Umair");
-            data.put("isNull", true);
-            data.put("addedOn", Timestamp.now());
-
-
-            db.collection("Brands")
-                    .document("ABC")
-                    .set(data)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-
-                            Toast.makeText(MainActivity.this, "Data Added", Toast.LENGTH_SHORT).show();
-                        }
-                    });
         });
 
 
